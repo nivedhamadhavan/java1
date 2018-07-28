c03a97b0614c875b3add1b3e2111d2a58a4b14f5
nivedhamadhavan <40656001+nivedhamadhavan@users.noreply.github.com>
2018-07-28 15:54:39 +0530

    Create ex9.java

---
+++ ex9.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ques9l2 {
+
+
+
+    public static void main(String[] args)
+
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input 1st integer: ");
+
+        int firstInt = sc.nextInt();
+
+        System.out.print("Input 2nd integer: ");
+
+        int secondInt = sc.nextInt();
+
+
+
+        System.out.printf("Sum of two integers: %d%n", firstInt + secondInt);
+
+        System.out.printf("Difference of two integers: %d%n", firstInt - secondInt);
+
+        System.out.printf("Product of two integers: %d%n", firstInt * secondInt);
+
+        System.out.printf("Average of two integers: %.2f%n", (double) (firstInt + secondInt) / 2);
+
+        System.out.printf("Distance of two integers: %d%n", Math.abs(firstInt - secondInt));
+
+        System.out.printf("Max integer: %d%n", Math.max(firstInt, secondInt));
+
+        System.out.printf("Min integer: %d%n", Math.min(firstInt, secondInt));
+
+    }
+
+}
